d53155088c2a332ce6cd6a58fc4efa8428f67b9c
Gyula Kádár <97313745+gyulakadar1983@users.noreply.github.com>
2025-04-13 04:57:33 +0300

    Specify base url in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "https://gyulakadar1983.github.io/mindbox-todo-list",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
